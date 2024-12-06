5c33334ef59a6dacd3f625dd58e8a086f89dcd13
Ayush Majumdar <theayushmajumdar@gmail.com>
2024-12-06 17:21:46 +0530

    updated the api url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://7o3ezcmqsffugwgqeew6ndfkrm0kellp.lambda-url.us-east-1.on.aws/task";
+export const API_URL = "https://4j6vkkieygsy6mlh3zmbi5lblq0expks.lambda-url.us-east-1.on.aws/task/";
